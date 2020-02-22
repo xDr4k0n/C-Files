48afd565ac2f4680ee2638081a61ac8e7ede9d22
Nicolae Olaru <61246526+xDr4k0n@users.noreply.github.com>
2020-02-22 23:21:28 +0100

    Add files via upload

---
+++ File_read_Decimal_basic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    FILE *file_pointer;
+    int carattere_singolo;
+    char nome_file[40];
+    printf("Inserisci nome del file : \n");
+    scanf("%s",nome_file);
+    file_pointer = fopen(nome_file,"r");
+    if(file_pointer == NULL)
+    {
+        printf("Errore nella lettura del File '%s'\n",nome_file);
+        return (-1);
+    }
+    //Lettura file decimale in Hex
+    while((carattere_singolo=getc(file_pointer)) != EOF)
+    {
+        printf("|%d|", carattere_singolo);
+    }
+    fclose(file_pointer);
+    file_pointer=NULL;
+    return 0;
+}
+
